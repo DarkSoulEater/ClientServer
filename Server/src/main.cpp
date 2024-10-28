67617d20ebdfde24775f1686ebfb13dc932e4b7a
DarkSoul <platonov.ta@phystech.edu>
2024-10-28 23:18:42 +0300

    Clean main

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,27 +1,8 @@
 #include "Server.hpp"
 #include "ParseArg.hpp"
-#include <string>
 
 int main(const int argc, const char* argv[]) {
     auto args = ParseArgs(argc, argv);
-    // auto sock = udp::Socket();
-    // if (sock < 0) {
-    //     perror("Socket:");
-    //     exit(1);
-    // }
-    // auto bind_st = udp::Bind(sock, args.port);
-    // if (bind_st < 0) {
-    //     perror("Bind:");
-    //     exit(1);
-    // }
-    // sockaddr_in addr;
-    // char* buff[1024] = {0};
-    // for (;;) {
-    //     auto res = udp::Recv(sock, buff, sizeof(buff), 0);
-    //     std::cout << std::string((char*)buff);
-    // }
-    // close(sock);
-    // return 0;
     Server server(args.proto, args.port);
     server.Start();
     return 0;
